02aacf5f3023b98abaefdd18ec73cf1cbc101fae
ShouxTech <70824139+ShouxTech@users.noreply.github.com>
2024-03-01 18:49:01 -0500

    Initial Commit

---
+++ DockLikeWindows/DockLikeWindowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockLikeWindowsApp.swift
+//  DockLikeWindows
+//
+//  Created by CriShoux on 2024-03-01.
+//
+
+import SwiftUI
+
+@main
+struct DockLikeWindowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
